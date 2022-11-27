491cfdd9d186cbe4f7c57db4b21fcd2f6d482bf2
Sebastian Pietras <spietras1998@gmail.com>
2022-11-27 14:07:53 +0100

    Configured blog

--- wordpress/scripts/setup/06-set-defaults.sh
+++ wordpress/scripts/setup/06-set-defaults.sh
@@ -9,5 +9,13 @@ if [ -z "$home" ]; then
   home="$(wp post list --post_type=page --name=home --field=ID)"
 fi
 
+blog="$(wp post list --post_type=page --name=blog --field=ID)"
+
+if [ -z "$blog" ]; then
+  wp post create --post_type=page --post_title='Blog' --post_status=publish
+  blog="$(wp post list --post_type=page --name=blog --field=ID)"
+fi
+
 [ "$(wp option get show_on_front)" == "page" ] || wp option update show_on_front page
 [ "$(wp option get page_on_front)" == "$home" ] || wp option update page_on_front "$home"
+[ "$(wp option get page_for_posts)" == "$blog" ] || wp option update page_for_posts "$blog"
