63dae948dbe02c98a31afcbf0786350f60fd619d
Sebastian Pietras <spietras1998@gmail.com>
2022-11-27 17:28:15 +0100

    Added updating custom theme and plugins at startup

--- wordpress/scripts/setup/04-fix-defaults.sh
+++ wordpress/scripts/setup/04-fix-defaults.sh
@@ -2,6 +2,10 @@
 
 echo "Fixing defaults..."
 
+wp plugin update ra-fix-rest
+wp plugin update ra-hide-admin-items
+wp plugin update ra-remove-redirects
+
 wp plugin is-active ra-fix-rest --quiet || wp plugin activate ra-fix-rest
 wp plugin is-active ra-hide-admin-items --quiet || wp plugin activate ra-hide-admin-items
 wp plugin is-active ra-remove-redirects --quiet || wp plugin activate ra-remove-redirects
--- wordpress/scripts/setup/05-register-custom-types.sh
+++ wordpress/scripts/setup/05-register-custom-types.sh
@@ -6,4 +6,6 @@ wp plugin is-installed advanced-custom-fields --quiet || wp plugin install advan
 
 wp plugin is-active advanced-custom-fields --quiet || wp plugin activate advanced-custom-fields
 
+wp plugin update ra-custom-types
+
 wp plugin is-active ra-custom-types --quiet || wp plugin activate ra-custom-types
