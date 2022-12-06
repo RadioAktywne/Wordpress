9d24dbf381858a27fe8fdf77eaf126c149b0a420
Sebastian Pietras <spietras1998@gmail.com>
2022-12-06 04:36:53 +0100

    Made it possible to change web port

--- web/server.js
+++ web/server.js
@@ -3,5 +3,7 @@ const http = require("http");
 
 const server = http.createServer(frontity);
 
-server.listen(3000);
-console.log("Listening on port 3000");
+const port = process.env.WEB_PORT || 3000;
+
+server.listen(port);
+console.log(`Server running at http://localhost:${port}`);
