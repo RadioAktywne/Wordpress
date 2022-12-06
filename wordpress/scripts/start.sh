aa371663ddaad2626cb27fdbaa137da5ff31eae7
Sebastian Pietras <spietras1998@gmail.com>
2022-12-06 04:36:38 +0100

    Cleaned up start script

--- wordpress/scripts/start.sh
+++ wordpress/scripts/start.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
 # https://wordpress.org/support/article/htaccess/
-if [ ! -e /usr/src/wordpress/.htaccess ]; then
-  {
-    echo '# BEGIN WordPress'
-    echo ''
-    echo 'RewriteEngine On'
-    echo 'RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]'
-    echo 'RewriteBase /'
-    echo 'RewriteRule ^index\.php$ - [L]'
-    echo 'RewriteCond %{REQUEST_FILENAME} !-f'
-    echo 'RewriteCond %{REQUEST_FILENAME} !-d'
-    echo 'RewriteRule . /index.php [L]'
-    echo ''
-    echo '# END WordPress'
-  } >.htaccess
-fi
+{
+  echo '# BEGIN WordPress'
+  echo ''
+  echo 'RewriteEngine On'
+  echo 'RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]'
+  echo 'RewriteBase /'
+  echo 'RewriteRule ^index\.php$ - [L]'
+  echo 'RewriteCond %{REQUEST_FILENAME} !-f'
+  echo 'RewriteCond %{REQUEST_FILENAME} !-d'
+  echo 'RewriteRule . /index.php [L]'
+  echo ''
+  echo '# END WordPress'
+} >.htaccess
 
 apache2-foreground &
 pid="$!"
 
 # wait until wordpress is ready
-until curl -sLf 'http://localhost:80' >/dev/null; do
+until curl -sLf 'http://localhost' >/dev/null; do
   echo "WordPress is unavailable - sleeping for 1 second"
   sleep 1
 done
@@ -32,7 +30,7 @@ if ! wp core is-installed --quiet; then
   echo "WordPress not yet installed. Installing..."
   ./scripts/install.sh || exit $?
 else
-  echo "WordPress is already installed. Url, title and admin user won't be changed even if configuration is different."
+  echo "WordPress is already installed."
 fi
 
 echo "Setting up..."
