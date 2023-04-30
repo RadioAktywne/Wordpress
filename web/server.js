1180861fc3d837dba7c869df2b6ada40d7d120ab
Sebastian Pietras <me@spietras.dev>
2023-04-30 20:43:52 +0200

    Updated versions of dependencies

--- web/server.js
+++ web/server.js
@@ -5,6 +5,11 @@ const server = http.createServer(frontity);
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+// Frontity is not maintained anymore and it uses old cryptographic libraries
+// They are not secure anymore, but we can't update them as their versions are constrained
+// So if we want to run with newer Node.js versions, we need to use the legacy provider
+process.env.NODE_OPTIONS = "--openssl-legacy-provider";
+
 const port = process.env.WEB_PORT || 3000;
 
 server.listen(port);
