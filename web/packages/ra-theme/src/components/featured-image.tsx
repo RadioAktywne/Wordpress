7d5755d3850496361cfba224bfbc5f98445156aa
74ovnikar73h <twojtek.ski@gmail.com>
2023-11-21 12:25:20 +0100

    another fallback fix

--- web/packages/ra-theme/src/components/featured-image.tsx
+++ web/packages/ra-theme/src/components/featured-image.tsx
@@ -47,7 +47,7 @@ function FeaturedImage({ id, size }: FeaturedImageProps): JSX.Element {
     <SquareContainer
       style={{
         background: "url(" + 
-          media.media_details.sizes[size] ? 
+            media && media.media_details && media.media_details.sizes[size] ? 
             (media.source_url.substring(0, media.source_url.lastIndexOf('/') + 1) + media.media_details.sizes[size].file)
             : (media.source_url)
            + ".webp)",
